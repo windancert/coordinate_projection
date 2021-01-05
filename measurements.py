d8f1b4e6c8d28ce73125e6c912e4b602b312e52d
Willem Jan Grootjans <willem.jan.grootjans@gmail.com>
2021-01-05 20:59:46 +0100

    Remeasure chair

--- measurements.py
+++ measurements.py
@@ -6,4 +6,4 @@ front_leg_left = ((0, 41, 0), (307, 973)) # linker voor poot
 back_leg = ((40, 1.5, 0), (626, 884)) # achter poot
 back_rest_right = ((49, -1.5, 103), (704, 245)) # rug leuning
 back_rest_left = ((49, 41.5, 103), (426, 232)) # rug leuning
-seat = ((14, 14, 48), (501, 736)) # zitvlak
+seat = ((14, 14, 45), (501, 736)) # zitvlak
